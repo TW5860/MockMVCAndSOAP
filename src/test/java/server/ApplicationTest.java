6460593c48e14c87d14f6c31129c4c8352eae478
Martin Soto <donsoto@gmail.com>
2018-06-13 17:39:20 +0200

    Create a working test.
    Not MockMVC, which actually doesn't work in this case, but a Spring
    integration test running off a local port.

--- src/test/java/server/ApplicationTest.java
+++ src/test/java/server/ApplicationTest.java
@@ -1,10 +1,5 @@
 package server;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,36 +8,28 @@ import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
-//@AutoConfigureMockMvc
-@ContextConfiguration(classes = WebServiceConfig.class)
-@WebAppConfiguration("server")
-@WebMvcTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
 	@Autowired
-	private MockMvc mockMvc;
-	
+	private WebTestClient webClient;
+
 	private String readResource(String name) {
 		InputStream inputStream = getClass().getResourceAsStream(name);
 		return new BufferedReader(new InputStreamReader(inputStream))
 		  .lines().collect(Collectors.joining("\n"));
 	}
 
-    @Test
-    public void canReadCountry() throws Exception {
+	@Test
+	public void exampleTest() {
     	String body = readResource("/request.xml");
-    	//System.out.println(body);
-        this.mockMvc.perform(post("/ws").header("content-type", "text/xml").content(body))
-        	.andExpect(status().isOk());
-            //.andExpect(content().string(containsString("Hello World")));
-    }
+    	this.webClient.post().uri("/ws").header("content-type", "text/xml")
+    		.body(BodyInserters.fromObject(body)).exchange().expectStatus().isOk();
+	}
 }
